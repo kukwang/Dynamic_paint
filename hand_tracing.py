4f8d21386e107e8de90b549105b6b1c36e3438ba
kukwang <skwang4010@naver.com>
2021-10-06 13:42:21 +0900

    remove useless function(find_distance)

--- hand_tracing.py
+++ hand_tracing.py
@@ -8,7 +8,6 @@ Modified by Kwangsoo Seol
 
 import cv2
 import mediapipe as mp
-import math
 
 
 # define hand detection class
@@ -93,20 +92,3 @@ class HandDetector:
 
         # return fingers information
         return fingers
-
-    # calculate distance between two points and draw line and circles
-    def find_distance(self, p1, p2, img, draw=True, r=15, t=3):
-        x1, y1 = self.lmList[p1][1:]
-        x2, y2 = self.lmList[p2][1:]
-        # mid point of two points
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-
-        if draw:
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
-            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
-        # calculate distance of two points
-        length = math.hypot(x2 - x1, y2 - y1)
-
-        return length, img, [x1, y1, x2, y2, cx, cy]
